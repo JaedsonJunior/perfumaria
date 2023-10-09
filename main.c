4dc973414ee78e9d8643ebf89b29944d007de2e1
@JaedsonJunior <jaedsonservice@gmail.com>
2023-10-09 19:25:13 -0300

    esqueleto da main

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-// Assinaturas
+#include "mdcliente.h"
+//Assinaturas
 int tela_menu_principal();
-
-
+void tela_cadastrar_cliente();
+void tela_pesquisar_cliente();
+void tela_alterar_cliente(); 
+void tela_excluir_cliente(); 
 
 /////
 // Programa principal
 int main() {
     int opcao=-1;
+    
     do {
         opcao=tela_menu_principal();
     } while (opcao != 0);
@@ -19,23 +22,24 @@ int main() {
 }
 int tela_menu_principal(void) {
     int opcao;
+    int opcaoCl=-1;
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
     printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
     printf("///               Departamento de Computação e Tecnologia                   ///\n");
     printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///                  Projeto Sistema de Gestão de perfumaria                 ///\n");
-    printf("///             Developed by @JaedsonJunior -- since Ago, 2023               ///\n");
+    printf("///                  Projeto Sistema de Gestão de perfumaria                ///\n");
+    printf("///             Developed by @JaedsonJunior -- since Ago, 2023              ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///            = = = = = Gestão de Perfumaria = = = = =                     ///\n");
     printf("///                                                                         ///\n");
-    printf("///            1. Módulo Cliente                                             ///\n");
-    printf("///            2. Módulo Produto                                             ///\n");
-    printf("///            3. Módulo funcionarios                                        ///\n");
-    printf("///            4. Módulo vendas                                              ///\n");
+    printf("///            1. Módulo Cliente                                            ///\n");
+    printf("///            2. Módulo Produto                                            ///\n");
+    printf("///            3. Módulo funcionarios                                       ///\n");
+    printf("///            4. Módulo vendas                                             ///\n");
     printf("///            5. Sobre                                                     ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
@@ -46,7 +50,10 @@ int tela_menu_principal(void) {
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     switch (opcao) {
             case 1:
-                printf("1 deu certo\n");
+            do
+            {
+            opcaoCl=tela_menu_cliente(); 
+            } while (opcaoCl!=0);
                 break;
             case 2:
                 printf("2 deu certo\n");
