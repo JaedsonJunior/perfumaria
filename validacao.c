158194b3f852e4cbd904c682d58abd7a1a109ea3
@JaedsonJunior <jaedsonservice@gmail.com>
2023-12-15 13:01:15 -0300

    besteriar

--- validacao.c
+++ validacao.c
@@ -15,7 +15,7 @@ bool validarCPF(const char *cpf){
     int i, j, digito1 = 0, digito2 = 0;
     if(strlen(cpf) != 11)
         return 0;
-    else if((strcmp(cpf,"00000000000") == 0) || (strcmp(cpf,"11111111111") == 0) || (strcmp(cpf,"22222222222") == 0) ||
+    else if((strcmp(cpf,"00000000000") == 0) || (strcmp(cpf,"22222222222") == 0) ||
             (strcmp(cpf,"33333333333") == 0) || (strcmp(cpf,"44444444444") == 0) || (strcmp(cpf,"55555555555") == 0) ||
             (strcmp(cpf,"66666666666") == 0) || (strcmp(cpf,"77777777777") == 0) || (strcmp(cpf,"88888888888") == 0) ||
             (strcmp(cpf,"99999999999") == 0))
